9f2d77c2988691147de2d42f3fd83eb47a7d662e
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-05-26 09:21:37 -0300

    referencing user posts

--- src/main/java/com/fernandosaviogo/workshopmongo/domain/User.java
+++ src/main/java/com/fernandosaviogo/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.fernandosaviogo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")   // Indica que a classe corresponde a uma coleção no MongoDB
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // DBRef faz a referencia para outra coleção no MongoDB e o (lazy = true) evita que a lista segua carregada quando referenciada
+	private List<Post> posts = new ArrayList<>();
 	
 	// Metodo construtor vazio
 	public User() {
@@ -53,6 +59,14 @@ public class User implements Serializable{
 		this.email = email;
 	}
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+		
 	// Metodo hashCode
 	@Override
 	public int hashCode() {
@@ -71,5 +85,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
