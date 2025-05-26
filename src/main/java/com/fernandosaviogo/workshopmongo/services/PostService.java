cc49cceaa583fa2fe1fcf200d3d86d940cdd2eb1
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-05-26 10:02:38 -0300

    Getting a post by ID

---
+++ src/main/java/com/fernandosaviogo/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.fernandosaviogo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernandosaviogo.workshopmongo.domain.Post;
+import com.fernandosaviogo.workshopmongo.repository.PostRepository;
+import com.fernandosaviogo.workshopmongo.services.exception.ObjectNotFoundException;
+
+// Anotação para fazer a classe ser um serviço para injetar em outras classes
+@Service
+public class PostService {
+	
+	// Instancia o objeto na classe (injeção de dependencia automatica)
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+}
